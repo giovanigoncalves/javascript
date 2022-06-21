31a0eb009f9cf52e0b259cf19532e2fbcae3aeaf
Giovani G. Ribamar <42078063+giovanigoncalves@users.noreply.github.com>
2022-06-21 01:15:58 -0300

    Início sobre loops (while e do while)

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,23 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+/*
+var n = 1
+while (n <= 6) {
+    console.log(`Passo ${n}`)
+    n++
+}
+*/
+
+/*
+console.log('Passo 1')
+console.log('Passo 2')
+console.log('Passo 3')
+console.log('Passo 4')
+console.log('Passo 5')
+console.log('Passo 6')
+*/
